8b9eb4c4b1cdc88a217fff39ba9d7297c771124f
xfyz_bv <xfy.incorp@gmail.com>
2024-12-23 22:23:30 -0400

    ajustando mas

--- ejemplodomjs/magenda/main.js
+++ ejemplodomjs/magenda/main.js
@@ -1,22 +1,50 @@
+// inicio de script
+var fullcontactos = [];
+var contacto = {};
 
-function cargardatos(){
-    const nombre = document.querySelector('.nombre')
-    const apellido = document.querySelector('.apellido')
-    const numero = document.querySelector('.numero')
-    const direccion = document.querySelector('.direccion')
-    const btn_formulario = document.querySelector('.btn_formulario') 
-    var dato = {
-        nombre: nombre.value,
-        apellido: apellido.value,
-        numero: numero.value,
-        direccion: direccion.value
+function llenardatos(){
+    if (window.localStorage.getItem("fcontactos")!= null){
+        fullcontactos = window.localStorage.getItem("fcontactos");
+        if (fullcontactos.length > 0){
+        
+        for (const i in fullcontactos) {
+            let contact = JSON.parse(fullcontactos[i]); 
+            let datafila = "<div>";
+            datafila += "<h3>" + contact.nombre + "</h3>";
+            datafila += "<h3>" + contact.apellido + "</h3>";
+            datafila += "<p>" + contact.numero + "</p>";
+            datafila += "<p>" + contact.direccion + "</p>";
+            datafila += "<span class='elim' onclick=eliminar()>Eliminar</span>";
+            datafila += "</div>";
+
+        }
+
+        document.getElementById(".listado").innerHTML = datafila;
     }
-console.log("lectura de carga")
+}
+    else {
+        console.log("lectura de carga")
 
-window.localStorage.setItem("inicio",dato.nombre,dato.apellido,dato.numero);
+//window.localStorage.setItem("Fcontactos",contacto.nombre);
 
-console.log(nombre.value,apellido.value,numero.value);
+//console.log(nombre.value,apellido.value,numero.value);
 
-console.log(window.localStorage.getItem("inicio"));
+console.log(window.localStorage.getItem("fcontactosag"));
+    }
+
 }
 
+function guardardatos(){
+    
+    contacto.nombre = document.getElementById('nombre').value;
+    contacto.apellido = document.getElementById('apellido').value;
+    contacto.numero = document.getElementById('numero').value;
+    contacto.direccion = document.getElementById('direccion').value;
+
+    fullcontactos.push(contacto);
+
+    console.log(contacto);
+    console.log(fullcontactos);
+    window.localStorage.setItem("fcontactos",JSON.stringify(fullcontactos));
+    llenardatos();
+}
